d833e3162fdc230573b608bf72eaeb343d89eb35
Dimas Fitrio Kurniawan <dimas.kurniawan@m3.formulatrix.com>
2024-04-04 18:46:28 +0700

    add role and user controller

---
+++ Backend/Controllers/RoleController.cs
@@ -0,0 +1,92 @@
+using AutoMapper;
+using Backend.DTOs;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoleController : ControllerBase
+{
+    private readonly ILogger<RoleController> _logger;
+    private readonly UserManager<User> _userManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IMapper _mapper;
+
+    public RoleController(
+        ILogger<RoleController> logger,
+        UserManager<User> userManager,
+        RoleManager<IdentityRole> roleManager,
+        IMapper mapper
+    )
+    {
+        _logger = logger;
+        _userManager = userManager;
+        _roleManager = roleManager;
+        _mapper = mapper;
+    }
+
+    [HttpGet()]
+    public ActionResult<IEnumerable<RoleDTO>> GetAllRole()
+    {
+        var item = _roleManager.Roles;
+        return Ok(_mapper.Map<IEnumerable<RoleDTO>>(item));
+    }
+
+    [HttpGet("{roleId}")]
+    public async Task<ActionResult<RoleDTO?>> GetRoleByIdAsync(string roleId)
+    {
+        var item = await _roleManager.FindByIdAsync(roleId);
+        return item is not null ? Ok(_mapper.Map<RoleDTO>(item)) : NotFound();
+    }
+
+    [HttpPost()]
+    public async Task<ActionResult<IdentityResult>> CreateRole(RoleDTO newRole)
+    {
+        var newItem = new IdentityRole()
+        {
+            Name = newRole.Name
+        };
+        var result = await _roleManager.CreateAsync(newItem);
+        if(result.Succeeded)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+
+    [HttpPost("{roleId}")]
+    public async Task<ActionResult<IdentityResult>> UpdateRole(string roleId, RoleDTO updatedRole)
+    {
+        IdentityResult result = new IdentityResult();
+        var existingRole = await _roleManager.FindByIdAsync(roleId);
+        if(existingRole is not null)
+        {
+            existingRole.Name = updatedRole.Name;
+            existingRole.NormalizedName = updatedRole.Name.ToUpper();
+            result = await _roleManager.UpdateAsync(existingRole);
+        }
+        if(result.Succeeded)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+
+    [HttpDelete("{roleId}")]
+    public async Task<ActionResult<IdentityResult>> DeleteRole(string roleId)
+    {
+        var deletedItem = await _roleManager.FindByIdAsync(roleId);
+        IdentityResult result = new IdentityResult();
+        if(deletedItem is not null && _userManager.GetUsersInRoleAsync(deletedItem.Name!).Result.Count.Equals(0))
+        {
+            result = await _roleManager.DeleteAsync(deletedItem);
+        }
+        if(result.Succeeded)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+}
--- Backend/Controllers/UserController.cs
+++ Backend/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-using Microsoft.AspNetCore.Authorization;
+using AutoMapper;
+using Backend.DTOs;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,16 +11,86 @@ public class UserController : ControllerBase
 {
     private readonly ILogger<UserController> _logger;
     private readonly UserManager<User> _userManager;
-    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly IMapper _mapper;
 
     public UserController(
         ILogger<UserController> logger,
         UserManager<User> userManager,
-        RoleManager<IdentityRole> roleManager
+        IMapper mapper
     )
     {
         _logger = logger;
         _userManager = userManager;
-        _roleManager = roleManager;
+        _mapper = mapper;
+    }
+
+     [HttpGet()]
+    public ActionResult<IEnumerable<UserDTO>> GetAllUser()
+    {
+        var item = _userManager.Users;
+        return Ok(_mapper.Map<IEnumerable<UserDTO>>(item));
+    }
+
+    [HttpGet("{userId}")]
+    public async Task<ActionResult<UserDTO?>> GetUserByIdAsync(string userId)
+    {
+        var item = await _userManager.FindByIdAsync(userId);
+        return item is not null ? Ok(_mapper.Map<UserDTO>(item)) : NotFound();
+    }
+
+    [HttpPost()]
+    public async Task<ActionResult<IdentityResult>> CreateUser(NewUserDTO newUser)
+    {
+        var newItem = new User()
+        {
+            UserName = newUser.UserName,
+            FirstName = newUser.FirstName,
+            LastName = newUser.LastName,
+            Email = newUser.Email,
+            EmailConfirmed = true
+
+        };
+        var result = await _userManager.CreateAsync(newItem, newUser.Password);
+        if(result.Succeeded)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+
+    [HttpPost("{userId}")]
+    public async Task<ActionResult<IdentityResult>> UpdateUser(string userId, UserDTO updatedUser)
+    {
+        IdentityResult result = new IdentityResult();
+        var existingUser = await _userManager.FindByIdAsync(userId);
+        if(existingUser is not null)
+        {
+            existingUser.FirstName = updatedUser.FirstName;
+            existingUser.LastName = updatedUser.LastName;
+            existingUser.Email = updatedUser.Email;
+            existingUser.UserName = updatedUser.UserName;
+            result = await _userManager.UpdateAsync(existingUser);
+        }
+        if(result.Succeeded)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
+    }
+
+    [HttpDelete("{userId}")]
+    public async Task<ActionResult<IdentityResult>> DeleteUser(string userId)
+    {
+        var deletedItem = await _userManager.FindByIdAsync(userId);
+        IdentityResult result = new IdentityResult();
+        if(deletedItem is not null)
+        {
+            result = await _userManager.DeleteAsync(deletedItem);
+        }
+        if(result.Succeeded)
+        {
+            return Ok(result);
+        }
+        return BadRequest(result);
     }
 }
