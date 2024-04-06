25115da7250ac4a599e99fbfc89edab934c5c0f2
Dimas Fitrio Kurniawan <dimas.kurniawan@m3.formulatrix.com>
2024-04-06 18:39:47 +0700

    add api base controller

---
+++ Backend/Controllers/ApiBaseController.cs
@@ -0,0 +1,44 @@
+using Backend.Models;
+using Backend.Utilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Backend.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ApiBaseController<T, DTO> : ControllerBase where T : ApiBaseController<T, DTO> where DTO : class
+{
+    protected ILogger<T> Logger { get; }
+    private IResponseFactory<DTO> _responseFactory { get; set; }
+
+    public ApiBaseController( ILogger<T> logger, IResponseFactory<DTO> responseFactory)
+    {
+      Logger = logger;
+      _responseFactory = responseFactory;
+    }
+
+    protected ActionResult<Response<DTO>> GeneratedResponse( IEnumerable<DTO> data, string message)
+    {
+        int statusCode = StatusCodes.Status200OK;
+        if(!message.IsNullOrEmpty())
+        {
+            statusCode = StatusCodes.Status404NotFound;
+        }
+        return StatusCode(statusCode, _responseFactory.CreateResponse( data, message ));
+    }
+
+    protected ActionResult<Response<DTO>> GeneratedResponse( DTO? data, string message)
+    {
+        int statusCode = StatusCodes.Status200OK;
+        if(!message.IsNullOrEmpty())
+        {
+            statusCode = StatusCodes.Status404NotFound;
+        }
+        if(data is null)
+        {
+            statusCode = StatusCodes.Status500InternalServerError;
+        }
+        return StatusCode(statusCode, _responseFactory.CreateResponse( data, message ));
+    }
+}
