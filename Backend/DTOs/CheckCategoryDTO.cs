16990efd9dc0862f887adb7a41dead9d6de0e31a
Dimas Fitrio Kurniawan <dimas.kurniawan@m3.formulatrix.com>
2024-04-07 15:20:51 +0700

    change check and sample category DTO property

--- Backend/DTOs/CheckCategoryDTO.cs
+++ Backend/DTOs/CheckCategoryDTO.cs
@@ -3,5 +3,5 @@ namespace Backend.DTOs;
 public class CheckCategoryDTO : DTOBase
 {
     public string Name { get; set; } = null!;
-    public IEnumerable<CheckServiceDTO> CheckServices { get; set; } = null!;
+    public IEnumerable<CheckServiceDTO>? CheckServices { get; set; }
 }
--- Backend/DTOs/SampleCategoryDTO.cs
+++ Backend/DTOs/SampleCategoryDTO.cs
@@ -3,5 +3,5 @@ namespace Backend.DTOs;
 public class SampleCategoryDTO : DTOBase
 {
     public string Name { get; set; } = null!;
-    public IEnumerable<SampleServiceDTO> SampleServices { get; set; } = null!;
+    public IEnumerable<SampleServiceDTO>? SampleServices { get; set; }
 }
