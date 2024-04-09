d8b1827d7fbbd1bd134db1f24689bb62a12a2df1
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-09 10:40:35 +0700

    add api call method

---
+++ Frontend/Frontend/utilities/api_call.py
@@ -0,0 +1,16 @@
+import httpx
+
+async def get(url: str) -> httpx.Response:
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url)
+        return response
+
+async def post(url: str, payload: dict) -> httpx.Response:
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, json=payload)
+        return response
+
+async def post(url: str) -> httpx.Response:
+    async with httpx.AsyncClient() as client:
+        response = await client.delete(url)
+        return response
