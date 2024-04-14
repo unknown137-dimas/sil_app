9e5581af0c0e7e446660d23d293c06ab3c8d4c8f
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-14 16:29:33 +0700

    add ability to select role row and get its data

--- Frontend/Frontend/pages/role.py
+++ Frontend/Frontend/pages/role.py
@@ -9,19 +9,45 @@ from pandas import DataFrame
 import reflex as rx
 
 class RoleState(rx.State):
-    roleData: DataFrame
+    columns: list
+    data: list
+    rawData: list
+    selectedRow: int
 
     async def getRole(self):
         response = await api_call.get(API_ROLE)
-        converter.to_data_table(loads(response.text)["data"])
-        self.roleData = converter.to_data_table(loads(response.text)["data"])
+        self.rawData = loads(response.text)["data"]
+        self.columns, self.data = converter.to_data_table(self.rawData)
+
+    def getData(self, pos):
+        _, self.selectedRow = pos
+    
+    def printData(self):
+        print(self.rawData[self.selectedRow])
 
 
 @template(route="/role", title="Role", image="/github.svg")
 def role() -> rx.Component:
     return rx.vstack(
-        rx.data_table(
-            data=RoleState.roleData,
-            on_mount=RoleState.getRole(),
-        )
+        rx.context_menu.root(
+            rx.context_menu.trigger(
+                rx.card(
+                    rx.data_editor(
+                        columns=RoleState.columns,
+                        data=RoleState.data,
+                        on_cell_context_menu=RoleState.getData,
+                        column_select="none",
+                    )
+                )
+            ),
+            rx.context_menu.content(
+                rx.context_menu.item("Edit",
+                on_click=RoleState.printData),
+                rx.context_menu.separator(),
+                rx.context_menu.item(
+                    "Delete", color="red",
+                ),
+            ),
+        ),
+        on_mount=RoleState.getRole
     )
