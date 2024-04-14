358e337c4b6cf2d8bcece80986af243a34747a36
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-14 09:17:19 +0700

    add login DTO

---
+++ Backend/DTOs/LoginCredentialDTO.cs
@@ -0,0 +1,6 @@
+namespace Backend.DTOs;
+
+public class LoginCredentialDTO
+{
+    public string Token { get; set; } = null!;
+}
---
+++ Backend/DTOs/LoginDTO.cs
@@ -0,0 +1,7 @@
+namespace Backend.DTOs;
+
+public class LoginDTO
+{
+    public string UserName { get; set; } = null!;
+    public string Password { get; set; } = null!;
+}
