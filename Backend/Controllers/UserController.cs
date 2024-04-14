c98228922b55f7812672ba3a762632e81a0367ad
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-14 09:25:12 +0700

    add assign role and sign in endpoint

--- Backend/Controllers/UserController.cs
+++ Backend/Controllers/UserController.cs
@@ -2,6 +2,7 @@ using AutoMapper;
 using Backend.DTOs;
 using Backend.Models;
 using Backend.Modules;
+using Backend.Services;
 using Backend.Utilities;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -13,23 +14,35 @@ namespace Backend.Controllers;
 public class UserController : ApiBaseController<UserController, UserDTO>
 {
     private readonly UserManager<User> _userManager;
+    private readonly SignInManager<User> _signInManager;
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly TokenService _tokenService;
     private readonly IMapper _mapper;
+    private readonly IResponseFactory<LoginCredentialDTO> _loginResponseFactory;
     private readonly IRelationCheckerModule _relationCheckerModule;
 
     public UserController(
         ILogger<UserController> logger,
         UserManager<User> userManager,
+        SignInManager<User> signInManager,
+        RoleManager<IdentityRole> roleManager,
+        TokenService tokenService,
         IMapper mapper,
         IResponseFactory<UserDTO> responseFactory,
+        IResponseFactory<LoginCredentialDTO> loginResponseFactory,
         IRelationCheckerModule relationCheckerModule
     ) : base(logger, responseFactory)
     {
         _userManager = userManager;
+        _signInManager = signInManager;
+        _roleManager = roleManager;
+        _tokenService = tokenService;
         _mapper = mapper;
+        _loginResponseFactory = loginResponseFactory;
         _relationCheckerModule = relationCheckerModule;
     }
 
-     [HttpGet()]
+    [HttpGet()]
     public ActionResult<Response<UserDTO>> GetAllUser()
     {
         var item = _mapper.Map<IEnumerable<UserDTO>>(_userManager.Users);
@@ -65,6 +78,27 @@ public class UserController : ApiBaseController<UserController, UserDTO>
         return GeneratedResponse(null, result.Errors);
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<Response<LoginCredentialDTO>>> Login(LoginDTO loginDTO)
+    {
+        var result = new Microsoft.AspNetCore.Identity.SignInResult();
+        var userLogin = await _userManager.FindByNameAsync(loginDTO.UserName);
+        if(userLogin is not null)
+        {
+            result = await _signInManager.PasswordSignInAsync(userLogin, loginDTO.Password, false, false);
+        }
+        if(result.Succeeded)
+        {
+            var userRole = await _userManager.GetRolesAsync(userLogin!);
+            var loginInfo = new LoginCredentialDTO()
+            {
+                Token = _tokenService.GenerateToken(userLogin!, userRole)
+            };
+            return GeneratedResponse(loginInfo, "", _loginResponseFactory);
+        }
+        return GeneratedResponse(null, "Error login", _loginResponseFactory);
+    }
+
     [HttpPost("{userId}")]
     public async Task<ActionResult<Response<UserDTO>>> UpdateUser(string userId, UserDTO updatedUser)
     {
@@ -77,6 +111,24 @@ public class UserController : ApiBaseController<UserController, UserDTO>
             existingUser.Email = updatedUser.Email;
             existingUser.UserName = updatedUser.UserName;
             result = await _userManager.UpdateAsync(existingUser);
+        }
+        if(result.Succeeded)
+        {
+            var userUpdated = _mapper.Map<UserDTO>(await _userManager.FindByIdAsync(userId));
+            return GeneratedResponse(userUpdated, result.Errors);
+        }
+        return GeneratedResponse(null, result.Errors);
+    }
+
+    [HttpPost("{userId}/assign/{roleId}")]
+    public async Task<ActionResult<Response<UserDTO>>> AssignRole(string userId, string roleId)
+    {
+        IdentityResult result = new IdentityResult();
+        var existingUser = await _userManager.FindByIdAsync(userId);
+        var role = await _roleManager.FindByIdAsync(roleId);
+        if(existingUser is not null && role is not null)
+        {
+            result = await _userManager.AddToRoleAsync(existingUser, role.ToString());
         }
         if(result.Succeeded)
         {
