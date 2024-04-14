970ce03eac54f28c6ffe8aade210f5b93a51b4ae
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-14 09:25:40 +0700

    add generic version of GeneratedResponse method

--- Backend/Controllers/ApiBaseController.cs
+++ Backend/Controllers/ApiBaseController.cs
@@ -47,6 +47,20 @@ public class ApiBaseController<T, DTO> : ControllerBase where T : ApiBaseControl
         return StatusCode(statusCode, _responseFactory.CreateResponse( data, message ));
     }
 
+    protected ActionResult<Response<T_DTO>> GeneratedResponse<T_DTO>( T_DTO? data, string message, IResponseFactory<T_DTO> responseFactory)
+    {
+        int statusCode = StatusCodes.Status200OK;
+        if(!message.IsNullOrEmpty())
+        {
+            statusCode = StatusCodes.Status404NotFound;
+        }
+        if(data is null)
+        {
+            statusCode = StatusCodes.Status500InternalServerError;
+        }
+        return StatusCode(statusCode, responseFactory.CreateResponse( data, message ));
+    }
+
     protected ActionResult<Response<DTO>> GeneratedResponse( DTO? data, IEnumerable<IdentityError> message)
     {
         int statusCode = StatusCodes.Status200OK;
