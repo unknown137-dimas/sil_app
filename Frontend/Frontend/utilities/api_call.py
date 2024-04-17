80bf04551c93497dc38d732b89a4ff1c5b17b38d
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-17 22:57:52 +0700

    convert payload to dictionary before send to api

--- Frontend/Frontend/utilities/api_call.py
+++ Frontend/Frontend/utilities/api_call.py
@@ -7,7 +7,7 @@ async def get(url: str) -> httpx.Response:
 
 async def post(url: str, payload: dict) -> httpx.Response:
     async with httpx.AsyncClient() as client:
-        response = await client.post(url, json=payload)
+        response = await client.post(url, json=dict(payload))
         return response
 
 async def delete(url: str) -> httpx.Response:
