7ada9884a37bf04bf909582016e19fabea762b1d
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-17 20:54:59 +0700

    add optional DTO property

--- Backend/DTOs/MedicalToolDTO.cs
+++ Backend/DTOs/MedicalToolDTO.cs
@@ -6,5 +6,8 @@ public class MedicalToolDTO : DTOBase
 {
     public string Name { get; set; } = null!;
     public string Code { get; set; } = null!;
+    public DateTime LastCalibrationDate { get; set; }
+    public DateTime CalibrationDate { get; set; }
     public CalibrationStatus CalibrationStatus { get; set; } = CalibrationStatus.NotOptimal;
+    public string? CalibrationNote { get; set; }
 }
