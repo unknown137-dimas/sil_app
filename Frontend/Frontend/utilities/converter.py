a864b9acd8d27a885ce5b08968dddf9b3bc5ce40
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-18 11:01:07 +0700

    add date format converter

--- Frontend/Frontend/utilities/converter.py
+++ Frontend/Frontend/utilities/converter.py
@@ -1,4 +1,5 @@
-from pandas import DataFrame
+from pandas import DataFrame, to_datetime
+from datetime import datetime
 
 def to_data_table(input_data: list) -> (list, list, DataFrame):
 
@@ -9,6 +10,12 @@ def to_data_table(input_data: list) -> (list, list, DataFrame):
     table_columns += [column for column in list(input_data[0].keys()) if column not in ignored_columns]
 
     dataframe = DataFrame(input_data, columns=table_columns)
+    
+    for column in dataframe.columns:
+        if "Date" in column:
+            dataframe[column] = to_datetime(dataframe[column], format="mixed")
+            dataframe[column] = dataframe[column].dt.strftime("%d %B %Y")
+
     dataframe["no"] = [i+1 for i in range(len(input_data))]
     dataframe.columns = dataframe.columns.str.capitalize()
 
@@ -23,3 +30,6 @@ def to_data_table(input_data: list) -> (list, list, DataFrame):
         columns.append(column)
 
     return columns, data, dataframe
+
+def to_date_only(date_string: str) -> str:
+    return datetime.fromisoformat(date_string).date().strftime("%Y-%m-%d")
