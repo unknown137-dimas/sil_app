4f5607f731974f47b58c7ef730c89e625a2f16b4
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-04-18 11:49:23 +0700

    add converter method and change to_data_table method logic

--- Frontend/Frontend/utilities/converter.py
+++ Frontend/Frontend/utilities/converter.py
@@ -1,13 +1,13 @@
 from pandas import DataFrame, to_datetime
 from datetime import datetime
+import re
 
 def to_data_table(input_data: list) -> (list, list, DataFrame):
 
     if(input_data == []):
         return [], [], None
     ignored_columns = ["id"]
-    table_columns = ["no"]
-    table_columns += [column for column in list(input_data[0].keys()) if column not in ignored_columns]
+    table_columns = [column for column in list(input_data[0].keys()) if column not in ignored_columns]
 
     dataframe = DataFrame(input_data, columns=table_columns)
     
@@ -16,20 +16,23 @@ def to_data_table(input_data: list) -> (list, list, DataFrame):
             dataframe[column] = to_datetime(dataframe[column], format="mixed")
             dataframe[column] = dataframe[column].dt.strftime("%d %B %Y")
 
-    dataframe["no"] = [i+1 for i in range(len(input_data))]
-    dataframe.columns = dataframe.columns.str.capitalize()
+    dataframe.insert(0, "no", [i+1 for i in range(len(input_data))])
 
     columns = []
     data = dataframe.values.tolist()
     
     for columnTitle, columnType in zip(dataframe.columns.tolist(), [type(x).__name__ for x in data[0]]):
         column = {
-            "title": columnTitle,
+            "title": to_title_case(columnTitle),
             "type": columnType,
         }
         columns.append(column)
 
+
     return columns, data, dataframe
 
-def to_date_only(date_string: str) -> str:
+def to_date_input(date_string: str) -> str:
     return datetime.fromisoformat(date_string).date().strftime("%Y-%m-%d")
+
+def to_title_case(input: str) -> str:
+    return re.sub(r"([A-Z])", r" \1", input).title().strip()
