215815301ae8a49411fd938bffd7f3497cf4646e
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-05-04 13:40:30 +0700

    add datetime constant

--- Frontend/Frontend/const/common_variables.py
+++ Frontend/Frontend/const/common_variables.py
@@ -2,3 +2,4 @@ from datetime import datetime
 
 TODAY_DATE_ONLY=datetime.now().strftime("%Y-%m-%d")
 TODAY_TIME_ONLY=datetime.now().strftime("%H:%M")
+TODAY_DATETIME=datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
