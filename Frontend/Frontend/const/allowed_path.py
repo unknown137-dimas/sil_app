7c457c0d0c966afabf3af560264db949831254a4
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-05-18 11:47:59 +0700

    change allowed path

--- Frontend/Frontend/const/allowed_path.py
+++ Frontend/Frontend/const/allowed_path.py
@@ -1,5 +1,8 @@
+from reflex.page import get_decorated_pages
+
 path_config = {
-    "Lab Staff": ["/patient_check"],
+    "Administrator": [page["route"] for page in get_decorated_pages() if page["route"] != "/login"],
+    "Lab Staff": ["/patient_check", "/medical_tool", "/reagen", "/patient"],
     "Sampling Staff": ["/patient_sample"],
-    "Registration Staff": ["/patient"],
+    "Registration Staff": ["/patient", "/patient_sample", "/patient_check"],
 }
