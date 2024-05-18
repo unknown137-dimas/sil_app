812340f0ebfc65fdf37e7c74ca8cd27508ed4530
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-05-18 11:48:56 +0700

    update enum

--- Frontend/Frontend/enum/enums.py
+++ Frontend/Frontend/enum/enums.py
@@ -18,27 +18,27 @@ class Gender(IntEnum):
     Female = 1
 
 class CalibrationStatus(IntEnum):
-    Good = 0,
-    NotOptimal = 1,
-    Calibrating = 2,
-    WaitingCalibration = 3,
+    Good = 0
+    NotOptimal = 1
+    Calibrating = 2
+    WaitingCalibration = 3
 
 class ValueType(IntEnum):
-    Numeric = 0,
+    Numeric = 0
     Text = 1
 
 class CheckStatus(IntEnum):
-    Processing = 0,
-    Waiting = 1,
+    Processing = 0
+    Waiting = 1
     Done = 2
 
 class ValidationStatus(IntEnum):
-    WaitingValidation = 0,
-    Valid = 1,
+    WaitingValidation = 0
+    Valid = 1
     NotValid = 2
 
 class UserRoles(Enum):
-    Admin = "Administrator",
-    Regis = "Registration Staff",
-    Sampling = "Sampling Staff",
+    Admin = "Administrator"
+    Regis = "Registration Staff"
+    Sampling = "Sampling Staff"
     Lab = "Lab Staff"
