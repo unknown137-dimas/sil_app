2615e4a832027fa6109894300e8f9bb7c9c2d7f8
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-05-23 12:04:01 +0700

    change api url

--- Frontend/Frontend/const/api.py
+++ Frontend/Frontend/const/api.py
@@ -1,15 +1,22 @@
-API_HOST = "http://localhost:5056/api"
-API_USER = API_HOST + "/user"
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+load_dotenv(Path("../../.env"))
+
+API_HOST = os.getenv("API_HOST")
+API_URL = API_HOST if API_HOST else "http://localhost:5056" + "/api"
+API_USER = API_URL + "/user"
 API_USER_LOGIN = API_USER + "/login"
-API_ROLE = API_HOST + "/role"
-API_MEDICAL_TOOL = API_HOST + "/medical-tool"
-API_REAGEN = API_HOST + "/reagen"
-API_CHECK = API_HOST + "/check"
+API_ROLE = API_URL + "/role"
+API_MEDICAL_TOOL = API_URL + "/medical-tool"
+API_REAGEN = API_URL + "/reagen"
+API_CHECK = API_URL + "/check"
 API_CHECK_CATEGORY = API_CHECK + "/category"
 API_CHECK_SERVICE = API_CHECK + "/service"
-API_SAMPLE = API_HOST + "/sample"
+API_SAMPLE = API_URL + "/sample"
 API_SAMPLE_CATEGORY = API_SAMPLE + "/category"
 API_SAMPLE_SERVICE = API_SAMPLE + "/service"
-API_PATIENT = API_HOST + "/patient"
+API_PATIENT = API_URL + "/patient"
 API_PATIENT_CHECK = API_PATIENT + "/check"
 API_PATIENT_SAMPLE = API_PATIENT + "/sample"
