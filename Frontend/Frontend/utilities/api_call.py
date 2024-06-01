909eea9557757e4d73aefe278df97c75dee25d6a
Dimas Fitrio Kurniawan <dimasfitriokurniaw4n@gmail.com>
2024-06-01 14:04:28 +0700

    add raw support in api call utility

--- Frontend/Frontend/utilities/api_call.py
+++ Frontend/Frontend/utilities/api_call.py
@@ -1,10 +1,12 @@
 import httpx
 from json import loads
 
-async def get(url: str) -> tuple[list[str], list[dict]]:
+async def get(url: str, params: dict = {}, raw: bool = False) -> tuple[list[str], list[dict]]:
     print(url)
     async with httpx.AsyncClient() as client:
-        response = await client.get(url)
+        response = await client.get(url, params=params)
+        if raw:
+            return response
         json_response = loads(response.text)
         return json_response["messages"], json_response["data"]
 
